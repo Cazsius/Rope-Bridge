3fbc5ca6d8a4d7098ffefa8f85a23bcf6610323a
Lewis_McReu <wim.creuwels@hotmail.com>
2017-02-05 01:31:55 +0100

    Temp fix bounding box

--- src/java/com/mrtrollnugnug/ropebridge/blocks/BridgeSlab.java
+++ src/java/com/mrtrollnugnug/ropebridge/blocks/BridgeSlab.java
@@ -21,63 +21,75 @@ import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BridgeSlab extends BasicBlock {
-	protected static float slabHeight = 4.0F/16.0F;
-	private final AxisAlignedBB boundingBox;
-	public static final PropertyEnum<EnumType> TYPE = PropertyEnum.create("type", BridgeSlab.EnumType.class);
-	public BridgeSlab(String unlocalizedName, float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
-		super(unlocalizedName, Material.WOOD, 1.0F, 5.0F);
-		this.setSoundType(SoundType.WOOD);
-		this.setDefaultState(this.blockState.getBaseState().withProperty(TYPE, EnumType.OAK));
-		boundingBox = new AxisAlignedBB(minX, minY, minZ, maxX, maxY, maxZ);	
-	}
-	
-	@Override
-	public boolean isFullBlock(IBlockState state) {
-		return false;
-	}
-	
-	@Override
-	public boolean isOpaqueCube(IBlockState state) {
-		return false;
-	}
-	
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return boundingBox.offset(pos);
-	}
-		
-	
-	public boolean canDropFromExplosion(Explosion explosionIn) {
-        return true;
+public class BridgeSlab extends BasicBlock
+{
+    protected static float slabHeight = 4.0F / 16.0F;
+    private final AxisAlignedBB boundingBox;
+    public static final PropertyEnum<EnumType> TYPE = PropertyEnum.create("type", BridgeSlab.EnumType.class);
+
+    public BridgeSlab(String unlocalizedName, float minX, float minY, float minZ, float maxX, float maxY, float maxZ)
+    {
+        super(unlocalizedName, Material.WOOD, 1.0F, 5.0F);
+        this.setSoundType(SoundType.WOOD);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(TYPE, EnumType.OAK));
+        boundingBox = new AxisAlignedBB(minX, minY, minZ, maxX, maxY, maxZ);
     }
-	
-	public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player) {
-        return true;
-    }
-	
-	public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+
+    @Override
+    public boolean isFullBlock(IBlockState state)
+    {
         return false;
     }
-	
-	public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return BridgeSlabTest.AABB_BOTTOM_HALF;
+    }
+
+    public boolean canDropFromExplosion(Explosion explosionIn)
+    {
+        return true;
+    }
+
+    public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player)
+    {
+        return true;
+    }
+
+    public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player)
+    {
+        return false;
+    }
+
+    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
+    {
         List<ItemStack> ret = new java.util.ArrayList<ItemStack>();
         int meta = getMetaFromState(state);
-        int slabMeta = (meta-meta%2)/2;
+        int slabMeta = (meta - meta % 2) / 2;
         ret.add(new ItemStack(Blocks.WOODEN_SLAB, 1, slabMeta));
         ret.add(new ItemStack(Items.STRING, RANDOM.nextInt(2)));
         return ret;
     }
-	
-	public ItemStack getPickBlock(RayTraceResult target, World world, BlockPos pos) {
+
+    public ItemStack getPickBlock(RayTraceResult target, World world, BlockPos pos)
+    {
         return null;
     }
-	
-    public boolean isOpaqueCube() {
+
+    public boolean isOpaqueCube()
+    {
         return false;
     }
 
-    public boolean isFullCube() {
+    public boolean isFullCube()
+    {
         return false;
     }
 
@@ -85,90 +97,119 @@ public class BridgeSlab extends BasicBlock {
     {
         return 20;
     }
-    
-    public boolean isFlammable(IBlockAccess world, BlockPos pos, EnumFacing face) {
+
+    public boolean isFlammable(IBlockAccess world, BlockPos pos, EnumFacing face)
+    {
         return true;
     }
-    
-    public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing face) {
+
+    public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing face)
+    {
         return 5;
     }
-    
-    public boolean isFireSource(World world, BlockPos pos, EnumFacing side) {
+
+    public boolean isFireSource(World world, BlockPos pos, EnumFacing side)
+    {
         return false;
     }
-    
-    public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+
+    public boolean isPassable(IBlockAccess worldIn, BlockPos pos)
+    {
         return false;
     }
-    
-    public int getMobilityFlag() {
-    	return 1;
-    }
-    
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[] { TYPE });
-    }
-    
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-    	switch (meta) {
-    	case 0: { return getDefaultState().withProperty(TYPE, EnumType.OAK); }
-    	case 1: { return getDefaultState().withProperty(TYPE, EnumType.OAK_R); }
-    	case 2: { return getDefaultState().withProperty(TYPE, EnumType.SPRUCE); }
-    	case 3: { return getDefaultState().withProperty(TYPE, EnumType.SPRUCE_R); }
-    	case 4: { return getDefaultState().withProperty(TYPE, EnumType.BIRCH); }
-    	case 5: { return getDefaultState().withProperty(TYPE, EnumType.BIRCH_R); }
-    	case 6: { return getDefaultState().withProperty(TYPE, EnumType.JUNGLE); }
-    	case 7: { return getDefaultState().withProperty(TYPE, EnumType.JUNGLE_R); }
-    	case 8: { return getDefaultState().withProperty(TYPE, EnumType.ACACIA); }
-    	case 9: { return getDefaultState().withProperty(TYPE, EnumType.ACACIA_R); }
-    	case 10: { return getDefaultState().withProperty(TYPE, EnumType.BIG_OAK); }
-    	case 11: { return getDefaultState().withProperty(TYPE, EnumType.BIG_OAK_R); }
-    	default: { return getDefaultState().withProperty(TYPE, EnumType.OAK); }
-    	}
+
+    public int getMobilityFlag()
+    {
+        return 1;
     }
 
     @Override
-    public int getMetaFromState(IBlockState state) {
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] { TYPE });
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        switch (meta) {
+        case 0: {
+            return getDefaultState().withProperty(TYPE, EnumType.OAK);
+        }
+        case 1: {
+            return getDefaultState().withProperty(TYPE, EnumType.OAK_R);
+        }
+        case 2: {
+            return getDefaultState().withProperty(TYPE, EnumType.SPRUCE);
+        }
+        case 3: {
+            return getDefaultState().withProperty(TYPE, EnumType.SPRUCE_R);
+        }
+        case 4: {
+            return getDefaultState().withProperty(TYPE, EnumType.BIRCH);
+        }
+        case 5: {
+            return getDefaultState().withProperty(TYPE, EnumType.BIRCH_R);
+        }
+        case 6: {
+            return getDefaultState().withProperty(TYPE, EnumType.JUNGLE);
+        }
+        case 7: {
+            return getDefaultState().withProperty(TYPE, EnumType.JUNGLE_R);
+        }
+        case 8: {
+            return getDefaultState().withProperty(TYPE, EnumType.ACACIA);
+        }
+        case 9: {
+            return getDefaultState().withProperty(TYPE, EnumType.ACACIA_R);
+        }
+        case 10: {
+            return getDefaultState().withProperty(TYPE, EnumType.BIG_OAK);
+        }
+        case 11: {
+            return getDefaultState().withProperty(TYPE, EnumType.BIG_OAK_R);
+        }
+        default: {
+            return getDefaultState().withProperty(TYPE, EnumType.OAK);
+        }
+        }
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
         EnumType type = (EnumType) state.getValue(TYPE);
         return type.getID();
     }
-    
-    public enum EnumType implements IStringSerializable {
-        OAK(0, "oak"),
-        OAK_R(1, "oak_r"),
-        SPRUCE(2, "spruce"),
-        SPRUCE_R(3, "spruce_r"),
-        BIRCH(4, "birch"),
-        BIRCH_R(5, "birch_r"),
-        JUNGLE(6, "jungle"),
-        JUNGLE_R(7, "jungle_r"),
-        ACACIA(8, "acacia"),
-        ACACIA_R(9, "acacia_r"),
-        BIG_OAK(10, "big_oak"),
-        BIG_OAK_R(11, "big_oak_r");
+
+    public enum EnumType implements IStringSerializable
+    {
+        OAK(0, "oak"), OAK_R(1, "oak_r"), SPRUCE(2, "spruce"), SPRUCE_R(3, "spruce_r"), BIRCH(4, "birch"), BIRCH_R(5, "birch_r"), JUNGLE(6, "jungle"),
+        JUNGLE_R(7, "jungle_r"), ACACIA(8, "acacia"), ACACIA_R(9, "acacia_r"), BIG_OAK(10, "big_oak"), BIG_OAK_R(11, "big_oak_r");
 
         private int ID;
         private String name;
-        
-        private EnumType(int ID, String name) {
+
+        private EnumType(int ID, String name)
+        {
             this.ID = ID;
             this.name = name;
         }
-        
+
         @Override
-        public String getName() {
+        public String getName()
+        {
             return name;
         }
 
-        public int getID() {
+        public int getID()
+        {
             return ID;
         }
-        
+
         @Override
-        public String toString() {
+        public String toString()
+        {
             return getName();
         }
     }
