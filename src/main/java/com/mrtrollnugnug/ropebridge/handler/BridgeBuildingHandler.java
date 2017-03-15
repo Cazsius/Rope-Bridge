6436553754c9ec97a4ccb2eb85533b726f6a8728
LewisMcReu <lewis_mcreu@MacBook-Pro-van-Wim.local>
2017-03-15 15:36:32 +0100

    Remove extraneous import

--- src/main/java/com/mrtrollnugnug/ropebridge/handler/BridgeBuildingHandler.java
+++ src/main/java/com/mrtrollnugnug/ropebridge/handler/BridgeBuildingHandler.java
@@ -15,7 +15,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class BridgeBuildingHandler {
 	private static String woodSlab = "tile.woodSlab";
