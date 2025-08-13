d243136cfa21e7842eef57411f961bfa6dd12778
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-13 16:07:46 +0200

    Break rope ladders downwards

--- src/main/java/com/mrtrollnugnug/ropebridge/block/RopeLadderBlock.java
+++ src/main/java/com/mrtrollnugnug/ropebridge/block/RopeLadderBlock.java
@@ -3,10 +3,14 @@ package com.mrtrollnugnug.ropebridge.block;
 import com.mrtrollnugnug.ropebridge.handler.ConfigHandler;
 import com.mrtrollnugnug.ropebridge.handler.ContentHandler;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootParams.Builder;
@@ -29,6 +33,16 @@ public class RopeLadderBlock extends LadderBlock {
 	}
 
 	@Override
+	public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel,
+	                              BlockPos pCurrentPos, BlockPos pFacingPos) {
+		if (!canSurvive(pState, pLevel, pCurrentPos) && pLevel instanceof Level level && !pLevel.getBlockState(pCurrentPos.above()).is(this)) {
+			dropResources(pState, level, pCurrentPos, null, null, null, false);
+			pLevel.setBlock(pCurrentPos, Blocks.AIR.defaultBlockState(), 3);
+		}
+		return super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+	}
+
+	@Override
 	public boolean isLadder(BlockState state, LevelReader level, BlockPos pos, LivingEntity entity) {
 		return true;
 	}
@@ -37,7 +51,7 @@ public class RopeLadderBlock extends LadderBlock {
 	public List<ItemStack> getDrops(BlockState state, Builder builder) {
 		List<ItemStack> drops = new ArrayList<>();
 		drops.add(new ItemStack(ContentHandler.rope.get(), ConfigHandler.getRopePerLadder()));
-		drops.add(new ItemStack(slabSupplier.get(), ConfigHandler.getWoodPerLadder()));
+		drops.add(new ItemStack(getSlab(), ConfigHandler.getWoodPerLadder()));
 		return drops;
 	}
 }
